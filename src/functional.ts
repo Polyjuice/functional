e707d1461ecf0c2242103df5e2a9c19ea0dbde07
Joachim Wester <joachimwester@me.com>
2020-07-18 19:35:42 +0200

    Compiler work

--- src/functional.ts
+++ src/functional.ts
@@ -1,2 +1,2 @@
-export { map, filter, take, tail, init, cons, union, Cons, head, count, InsertAt, foldl, last,foldr, fold, makeMap } from "./list.js"
+export { _map, map, _mapEager, filter, take, tail, init, cons, union, Cons, head, count, InsertAt, foldl, last,foldr, fold, makeMap } from "./list.js"
 export { ᐅ, ᐊ } from "./compose.js";
